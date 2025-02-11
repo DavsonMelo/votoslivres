4328adfb55afb3f215e0417346335ec9aa94c75f
Davson <davson_torres@hotmail.com>
2025-02-11 01:37:48 -0300

    implementação gameover e restart game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,184 @@
 document.addEventListener('DOMContentLoaded', () => { // Garante que o DOM está carregado
     const startButton = document.querySelector('.neon-btn'); // Seleciona o botão Start
 
-    startButton.addEventListener('click', () => { // Adiciona o ouvinte de eventos
-        startButton.disabled = true; // Desativa o botão
-        startGame(); // Chama startGame() quando o botão é clicado
+    const urlParams = new URLSearchParams(window.location.search);
+    const restart = urlParams.get('restart');
+
+    if (restart === 'true') {
+        window.history.replaceState({}, document.title, window.location.pathname);
+        
+        startButton.disabled = true;
+        startGame();
+    }
+
+    startButton.addEventListener('click', () => { // Adiciona o ouvinte de eventos ao botão Start
+        startButton.disabled = true; // Desativa o botão Start após ser clicado
+        startGame(); // Chama a função startGame() quando o botão é clicado
     });
 
     function startGame() {
-        const policia = document.querySelector('.policia')
-        const picanha = document.querySelector('.picanha');
-        const cadeia = document.querySelector('.cadeia');
-        const dolares = document.querySelector('.dolares');
-        const mario = document.querySelector('.mario');
-        const cidade = document.querySelector('.cidade')
-        const cidade2 = document.querySelector('.cidade2')
-
-        const restartButton = document.querySelector('.neon-btn'); // Seleciona o botão Restart
-        
-        const scoreElement = document.getElementById('score');
-
-        const trilhasonora = document.getElementById('supermario');
-        const sirene = document.getElementById('sirene')
+        // Agrupa as seleções de elementos em um único objeto
+        const elements = {
+            policia: document.querySelector('.policia'), // Seleciona o elemento policia
+            picanha: document.querySelector('.picanha'), // Seleciona o elemento picanha
+            cadeia: document.querySelector('.cadeia'), // Seleciona o elemento cadeia
+            dolares: document.querySelector('.dolares'), // Seleciona o elemento dolares
+            mario: document.querySelector('.mario'), // Seleciona o elemento mario
+            cidade: document.querySelector('.cidade'), // Seleciona o elemento cidade
+            cidade2: document.querySelector('.cidade2'), // Seleciona o elemento cidade2
+            restartButton: document.querySelector('.neon-btn'), // Seleciona o botão Restart
+            scoreElement: document.getElementById('score'), // Seleciona o elemento de pontuação
+            trilhasonora: document.getElementById('supermario'), // Seleciona o elemento de áudio da trilha sonora
+            sirene: document.getElementById('sirene') // Seleciona o elemento de áudio da sirene
+        };
 
         const animations = [
-            { element: picanha, animationName: 'picanha-animation' },
-            { element: cadeia, animationName: 'cadeia-animation' },
-            { element: dolares, animationName: 'dolares-animation' },
+            { element: elements.picanha, animationName: 'picanha-animation' }, // Define a animação para picanha
+            { element: elements.cadeia, animationName: 'cadeia-animation' }, // Define a animação para cadeia
+            { element: elements.dolares, animationName: 'dolares-animation' }, // Define a animação para dolares
         ];
 
-        let animationSpeed = 3.2;
-        let score = 0;
-        let dollarCount = 0;
-        let endGame = false;
-        let isJumping = false;
-        trilhasonora.volume = 0.5;
-        sirene.volume = 0.06;
+        let animationSpeed = 3.2; // Define a velocidade padrão das animações
+        let score = 0; // Inicializa a pontuação
+        let dollarCount = 0; // Inicializa a contagem de dólares
+        let endGame = false; // Inicializa o estado do jogo como não terminado
+        let isJumping = false; // Inicializa o estado de pulo como falso
+        elements.trilhasonora.volume = 0.5; // Define o volume da trilha sonora
+        elements.sirene.volume = 0.06; // Define o volume da sirene
 
-        animatePolicia();
-        trilhasonora.play();
+        animatePolicia(); // Inicia a animação da polícia
+        elements.trilhasonora.play(); // Reproduz a trilha sonora
 
         function startAnimation(element, animationName) {
-            element.style.animation = 'none';
-            void element.offsetWidth;
-            element.style.animation = `${animationName} ${animationSpeed}s linear 1`;
+            element.style.animation = 'none'; // Remove qualquer animação existente
+            void element.offsetWidth; // Força um reflow para resetar a animação
+            element.style.animation = `${animationName} ${animationSpeed}s linear 1`; // Define a nova animação
         }
 
         function runAnimations() {
-            const randomIndex = Math.floor(Math.random() * animations.length);
-            const { element, animationName } = animations[randomIndex];
+            const randomIndex = Math.floor(Math.random() * animations.length); // Seleciona uma animação aleatória
+            const { element, animationName } = animations[randomIndex]; // Obtém o elemento e o nome da animação
 
-            startAnimation(element, animationName);
+            startAnimation(element, animationName); // Inicia a animação
 
             element.addEventListener('animationend', () => {
-                setTimeout(runAnimations, 500);
+                setTimeout(runAnimations, 500); // Reinicia a animação após um intervalo de 500ms
             }, { once: true });
         }
+
         function animatePolicia() {
-            policia.style.animation = 'none';
-            void policia.offsetWidth;
-            policia.style.animation = `policia-animation 5s linear 1`; // Ajuste o tempo (2s) conforme necessário
-            sirene.play();
+            elements.policia.style.animation = 'none'; // Remove qualquer animação existente
+            void elements.policia.offsetWidth; // Força um reflow para resetar a animação
+            elements.policia.style.animation = `policia-animation 5s linear 1`; // Define a animação da polícia
+            elements.sirene.play(); // Reproduz a sirene
           
-            policia.addEventListener('animationend', () => {
-                sirene.pause();
-                setTimeout(animatePolicia, 10000); // Espaçamento de 5 segundos (5000ms)
+            elements.policia.addEventListener('animationend', () => {
+                elements.sirene.pause(); // Pausa a sirene quando a animação termina
+                setTimeout(animatePolicia, 10000); // Reinicia a animação da polícia após 10 segundos
             }, { once: true });
-          }
+        }
 
-          let animacaoAtual = 1;
-          
-          function animateCidade() {
-            cidade.style.animation = 'none';
-            void cidade.offsetWidth;
-            cidade.style.animation = 'cidade-animation 25s linear 1';
+        let animacaoAtual = 1; // Inicializa a variável de controle de animação
+        function animateCidade() {
+            elements.cidade.style.animation = 'none'; // Remove qualquer animação existente
+            void elements.cidade.offsetWidth; // Força um reflow para resetar a animação
+            elements.cidade.style.animation = 'cidade-animation 25s linear 1'; // Define a animação da cidade
 
-            cidade.addEventListener('animationend', () => {
-                animacaoAtual = 2; // Próxima animação é a cidade2
-                animateCidade2();
+            elements.cidade.addEventListener('animationend', () => {
+                animacaoAtual = 2; // Define a próxima animação como cidade2
+                animateCidade2(); // Inicia a animação da cidade2
             }, { once: true });
-          }
-          function animateCidade2() {
-            cidade2.style.animation = 'none';
-            void cidade2.offsetWidth;
-            cidade2.style.animation = 'cidade2-animation 25s linear 1';
+        }
 
-            cidade2.addEventListener('animationend', () => {
-                animacaoAtual = 1; // Próxima animação é a cidade
-                animateCidade();
+        function animateCidade2() {
+            elements.cidade2.style.animation = 'none'; // Remove qualquer animação existente
+            void elements.cidade2.offsetWidth; // Força um reflow para resetar a animação
+            elements.cidade2.style.animation = 'cidade2-animation 25s linear 1'; // Define a animação da cidade2
+
+            elements.cidade2.addEventListener('animationend', () => {
+                animacaoAtual = 1; // Define a próxima animação como cidade
+                animateCidade(); // Inicia a animação da cidade
             }, { once: true });
-          }
-          animateCidade();
+        }
 
-        runAnimations(); // Inicia as animações
+        animateCidade(); // Inicia a animação da cidade
+
+        runAnimations(); // Inicia as animações dos elementos
+
+        function jump() {
+            if (!elements.mario.classList.contains('jump')) {
+                elements.mario.classList.add('jump');
+                isJumping = true;
+
+                setTimeout(() => {
+                    const jumpedOverPicanha = checkJumpOver(elements.picanha);
+                    const jumpedOverCadeia = checkJumpOver(elements.cadeia);
+        
+                    if (jumpedOverPicanha || jumpedOverCadeia) {
+                        score++; // Incrementa a pontuação
+                        updateScore();
+                    }
+                }, 350); // Verifica no meio do pulo
+
+                setTimeout(() => {
+                    elements.mario.classList.remove('jump');
+                    isJumping = false;
+                }, 700); // Duração da animação de pulo
+            }
+        }
+        document.addEventListener('keydown', (event) => {
+            if (event.code === 'Space') {
+                jump();
+            }
+        });
+
+        function checkCollision(element) {
+            const marioRect = elements.mario.getBoundingClientRect();
+            const elementRect = element.getBoundingClientRect();
+
+            if (
+                elementRect.left < marioRect.right &&
+                elementRect.right > marioRect.left &&
+                elementRect.top < marioRect.bottom &&
+                elementRect.bottom > marioRect.top
+            ) {
+                if (element === elements.picanha || element === elements.cadeia) {
+                    endGame = true;
+                    stopGame(); 
+                }else if (element === elements.dolares && isJumping) {
+                    endGame = true;
+                    stopGame();
+                }
+            }
+        }
+
+        function stopGame() {
+            animations.forEach(({ element }) => {
+                element.style.animation = 'none';
+            });
+            elements.mario.style.animation = 'none';
+            elements.mario.src = 'images/choro.png';
+            elements.trilhasonora.pause(); // Pausa a trilha sonora
+            elements.trilhasonora.currentTime = 0; // Reinicia a trilha sonora
+
+            document.getElementById('gameOverModal').style.display = 'block';
+
+        }
+
+        const loop = setInterval(() => {
+            animations.forEach(({ element }) => checkCollision(element));
+            if (endGame) clearInterval(loop);
+        }, 10);
     }
+
+    function resetGame() {
+        location.reload(); // Recarrega a página para reiniciar o jogo
+    }
+    document.getElementById('restartGame').addEventListener('click', () => {
+        window.location.href = window.location.pathname + "?restart=true"; // Recarrega a página com o parâmetro
+
+    });
+
+    // Remova o ouvinte de eventos de resetGame
+    // startButton.addEventListener('click', resetGame); 
 });
