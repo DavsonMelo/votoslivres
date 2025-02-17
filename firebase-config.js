087bb7372c69a294f731714fe2103b4510b7aa36
Davson S. Melo <davsonmelo@protonmail.com>
2025-02-16 22:35:20 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDA0W6y3EyJdwZLZ_ozUH-0mf-P4DvF3uM",
-    authDomain: "superinaciobro.firebaseapp.com",
-    databaseURL: "https://superinaciobro-default-rtdb.firebaseio.com",
-    projectId: "superinaciobro",
-    storageBucket: "superinaciobro.firebasestorage.app",
-    messagingSenderId: "1034535548084",
-    appId: "1:1034535548084:web:d2bacd4295e969308cd3e1"
+   
   };
   
   export default firebaseConfig;
